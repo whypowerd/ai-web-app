6d6e10e030925707355c33234a6807034054b0ac
whypowerd <whypowered@mail.com>
2025-02-01 00:21:21 -0600

    Update server configuration for better stability

--- backend/app.py
+++ backend/app.py
@@ -183,4 +183,4 @@ def get_whys():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)
